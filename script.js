b9f53d88ffd5a0160311ed28aab62a6d552c2c1c
Teke Byas <tekelamon01@gmail.com>
2023-03-30 18:56:53 -0700

    counter display updates

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-
+// initialize counter
+let counter = 0;
   
-//   TODO define a counter variable that is mutable
-
-  
+// attach to html counter
 let counterElement = document.getElementById("counter");
 
+// display current number
 function updateCounter() {
   counterElement.innerText = counter
 }
 
+// add 1
 function handleIncrement() {
-  // TODO update count by adding 1
-  
+  counter++;
   updateCounter();
 }
 
+// sub 1
 function handleDecrement() {
-  // TODO update count by subtracting 1
-  
+  counter--;
   updateCounter();
 }
 
+// whenever the buttons are clicked, perform related action
 let incrementButton = document.getElementById("increment-button");
 incrementButton.addEventListener("click", handleIncrement);
 
 let decrementButton = document.getElementById("decrement-button");
 decrementButton.addEventListener("click", handleDecrement);
 
+// display counter after initializing data
 updateCounter();
